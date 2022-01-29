5e08acf3db8aae3d083db1a6fac061bcc572826c
zzung00 <lee759444@gmail.com>
2022-01-29 18:26:49 +0900

    Initial Commit

---
+++ HeyTaxiDriver/HeyTaxiDriverApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeyTaxiDriverApp.swift
+//  HeyTaxiDriver
+//
+//  Created by 이정현 on 2022/01/29.
+//
+
+import SwiftUI
+
+@main
+struct HeyTaxiDriverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
