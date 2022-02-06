539da5bd1d51d189dc1f597a43e9b3e52a0be4e9
zzung00 <lee759444@gmail.com>
2022-02-06 17:03:31 +0900

    create keychain

---
+++ HeyTaxiDriver/Utils/TokenUtils.swift
@@ -0,0 +1,19 @@
+//
+//  TokenUtils.swift
+//  HeyTaxiDriver
+//
+//  Created by 이정현 on 2022/02/06.
+//
+
+import Foundation
+import Security
+import Alamofire
+
+class TokenUtils {
+    func save(service: String, account: String, value: String) {
+//        let keyChainQuery: NSDictionary = [
+//            kSecClass: kSecClassGenericPassword,
+//            kSecAttrService: service
+//        ]
+    }
+}
