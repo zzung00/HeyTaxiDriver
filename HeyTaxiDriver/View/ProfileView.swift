e34c6e35863fa70abf7cf3535ac76c68cb87b383
zzung00 <lee759444@gmail.com>
2022-02-10 18:13:19 +0900

    update profileView

--- HeyTaxiDriver/View/ProfileView.swift
+++ HeyTaxiDriver/View/ProfileView.swift
@@ -8,16 +8,43 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var carNumber = ""
+    
     var body: some View {
         NavigationView {
-            VStack {
+            VStack(alignment: .center, spacing: 30) {
                 //택시 등록 area
+//                Circle()
+//                    .fill(Color.mainGreen)
+//                    .frame(width: 150, height: 100)
+                
+                Image("taxi")
+                    .resizable()
+                    .renderingMode(.template)
+                    .foregroundColor(.white)
+                    .frame(width: 250, height: 250)
+                    .background(
+                        Circle()
+                            .fill(Color.mainGreen)
+                    )
+                    .overlay(
+                        Circle()
+                            .stroke(Color.white, lineWidth: 4)
+                    )
+                    .shadow(radius: 10)
+                
+                TextField("택시번호", text: $carNumber)
+                    .frame(width: 300, height: 50)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.default)
+                    .disableAutocorrection(true)
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarLeading) {
                     Button(action: {}) {
                         Text("취소")
                             .bold()
+                            .foregroundColor(.red)
                     }
                 }
                 
