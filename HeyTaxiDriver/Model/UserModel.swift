8a3c9ec2b9ebbb2ae30b41bf5bfd3b518b94f5f2
zzung00 <lee759444@gmail.com>
2022-04-10 17:10:48 +0900

    added param

--- HeyTaxiDriver/Model/UserModel.swift
+++ HeyTaxiDriver/Model/UserModel.swift
@@ -8,10 +8,12 @@
 import Foundation
 
 struct UserModel: Codable {
+    let id: Int
     let name: String?
     let username: String?
     
-    init(name: String, username: String) {
+    init(id: Int, name: String, username: String) {
+        self.id = id
         self.name = name
         self.username = username
     }
