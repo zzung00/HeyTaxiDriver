c6f55ef47fbc3979e5d96c0525b8c89b7f0d8ea0
zzung00 <lee759444@gmail.com>
2022-04-23 15:20:34 +0900

    update

--- HeyTaxiDriver/View/MainView.swift
+++ HeyTaxiDriver/View/MainView.swift
@@ -19,59 +19,59 @@ struct MainView: View {
                 //새로운 bottom sheet 생성 후, 기존 메인 화면에 빈차(empty)와 퇴근(off) 표시
                 //new sheet에 승객의 src, dst, 승차(reservation)와 하차(off) 표시
             }
-            
-            VStack(alignment: .center, spacing: 30) {
-                Button(action: {viewModel.setStatus(status: TaxiStatus.empty)}) {
-                    Text("빈 차")
-                        .frame(width: 220, height: 50)
+            else {
+                VStack(alignment: .center, spacing: 30) {
+                    Button(action: {viewModel.setStatus(status: TaxiStatus.empty)}) {
+                        Text("빈 차")
+                            .frame(width: 220, height: 50)
+                    }
+                    .background(Color.empty)
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+                    .font(Font.body.bold())
+                    .frame(width: 220)
+                    
+                    Button(action: {}) {
+                        Text("승차")
+                            .frame(width: 220, height: 50)
+                    }
+                    .background(Color.on)
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+                    .font(Font.body.bold())
+                    .frame(width: 220)
+                    
+                    Button(action: {viewModel.setStatus(status: TaxiStatus.off)}) {
+                        Text("퇴근")
+                            .frame(width: 220, height: 50)
+                    }
+                    .background(Color.off)
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+                    .font(Font.body.bold())
+                    .frame(width: 220)
                 }
-                .background(Color.empty)
-                .cornerRadius(10)
-                .foregroundColor(.white)
-                .font(Font.body.bold())
-                .frame(width: 220)
-                
-                Button(action: {}) {
-                    Text("승차")
-                        .frame(width: 220, height: 50)
-                }
-                .background(Color.on)
-                .cornerRadius(10)
-                .foregroundColor(.white)
-                .font(Font.body.bold())
-                .frame(width: 220)
-                
-                Button(action: {viewModel.setStatus(status: TaxiStatus.off)}) {
-                    Text("하차")
-                        .frame(width: 220, height: 50)
-                }
-                .background(Color.off)
-                .cornerRadius(10)
-                .foregroundColor(.white)
-                .font(Font.body.bold())
-                .frame(width: 220)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    NavigationLink(destination: ProfileView()) {
-                            Image(systemName: "person")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        NavigationLink(destination: ProfileView()) {
+                                Image(systemName: "person")
+                        }
                     }
                 }
+                .onAppear {
+                    viewModel.loadMe()
+                    viewModel.requestPermission()
+                    viewModel.registerSocket()
+                }
+                
+                .alert(isPresented: $viewModel.reserveAlert) {
+                    Alert(title: Text("HeyTaxi"), message: Text("콜 수락하시겠습니까?\n\r \(viewModel.address)"), primaryButton: .destructive(Text("수락"))
+                          {viewModel.allowReservation()}
+                          , secondaryButton: .cancel(Text("거절"))
+                          {viewModel.rejectReservation()}
+                    )
+                }
             }
-            .onAppear {
-                viewModel.loadMe()
-                viewModel.requestPermission()
-                viewModel.registerSocket()
-            }
-            
-            .alert(isPresented: $viewModel.reserveAlert) {
-                Alert(title: Text("HeyTaxi"), message: Text("콜 수락하시겠습니까?\n\r \(viewModel.address)"), primaryButton: .destructive(Text("수락"))
-                      {viewModel.allowReservation()}
-                      , secondaryButton: .cancel(Text("거절"))
-                      {viewModel.rejectReservation()}
-                )
-            }
-            
         }.navigationBarHidden(true)
     }
 }
